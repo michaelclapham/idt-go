fcb6a7ad150f1060a423c0eaed969096ed164e3d
Michael Clapham <michaeljclapham@gmail.com>
2021-06-10 16:07:44 +0100

    Command line parsing

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import "os"
 
 func main() {
-	fmt.Println("hello world")
+	// Use arguments to decide command and which file to apply to
+	if len(os.Args) < 3 {
+		println("Usage: idt-rust [COMMAND] [FILENAME]")
+		println("Example: idt-rust extract test.idml")
+		os.Exit(-1)
+	}
+
+	if os.Args[1] == "extract" {
+		extract(os.Args[2])
+	} else if os.Args[1] == "translate" {
+		translate(os.Args[2])
+	} else {
+		println("Unknown command", os.Args[1])
+	}
+}
+
+func extract(filename string) {
+	println("Extracting strings from ", filename)
+}
+
+func translate(filename string) {
+	println("Translating file ", filename)
 }
